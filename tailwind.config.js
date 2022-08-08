57e27a9607d9a89f2648df42de122749d6f21bc3
unknown <mfaqihuddin23@gmail.com>
2022-08-08 15:07:03 +0700

    about page input

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/**/*.{html,js}", "index.html", "./node_modules/flowbite/**/*.js"],
+  content: ["./assets/**/*.{html,js}", "*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       container: {
